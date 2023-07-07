9e3eb60fe9b967146101d3adb6df1b0b24cbf3ce
lionel <lionelhudson171@gmail.com>
2023-07-07 03:04:38 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import Form from './components/Form'
+import Header from './components/Header'
+import Layout from './components/Layout'
+import List from './components/List'
 
-import './App.css';
-import Layout from './components/Layout';
-import Header from './components/Header';
-import Form from './components/Form';
+const App = () => {
+  const [error, setError] = useState(null)
+  const [todo, setTodo] = useState('')
+  const [todos, setTodos] = useState([])
 
-function App() {
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos'))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  // saving the todos in browser storage to prevent loss of todos on refreshing tab
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    if (todo.length < 5) {
+      setError('At least 5 word required!')
+      return false
+    }
+
+    setTodos([{ id: Date.now(), title: todo, done: false }, ...todos])
+
+    setTodo('')
+    setError(null)
+  }
+
+  const delHandler = (todoId) => {
+    if (window.confirm('Are you sure')) {
+      const updatedTodos = todos.filter((item) => item.id !== todoId)
+
+      setTodos(updatedTodos)
+    }
+  }
+
+  const doneHandler = (todoId) => {
+    const index = todos.findIndex((emp) => emp.id === todoId)
+    const newTodo = [...todos]
+
+    newTodo[index] = {
+      id: todos[index].id,
+      title: todos[index].title,
+      done: !todos[index].done,
+    }
+
+    setTodos(newTodo)
+  }
+
   return (
-    <div className="App">
-      <Layout>
-        <Header />
-        <Form />
-      </Layout>
-    </div>
-  );
+    <Layout>
+      <Header />
+      <Form
+        error={error}
+        value={todo}
+        submit={submitHandler}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <hr className='border-primary' />
+      <List todos={todos} delHandler={delHandler} doneHandler={doneHandler} />
+    </Layout>
+  )
 }
 
-export default App;
+export default App
--- src/components/List.js
+++ src/components/List.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import ListItems from './ListItems';
+import React from 'react'
+import ListItems from './ListItems'
 
-const List = ({ todos, delHandler, doneHandler }) => {
-    if (todos.length <= 0) {
-        return (
-            <div className='jumbotron text-center'>
-                <h1 className='text-danger'>No Todo Found!</h1>
-                <h4>Add your own!</h4>
-            </div>
-        )
-    }
-
+const Lists = ({ todos, delHandler, doneHandler }) => {
+  if (todos.length <= 0) {
     return (
-        <div className='listWrap'>
-            <ul className='list-group'>
-                {todos &&
-                    todos.length > 0 &&
-                    todos.map((todo) => (
-                        <ListItems
-                            key={todo.id}
-                            id={todo.id}
-                            title={todo.title}
-                            done={todo.done}
-                            delHandler={delHandler}
-                            doneHandler={doneHandler}
-                        />
-                    ))}
-            </ul>
-        </div>
+      <div className='jumbotron text-center'>
+        <h1 className='text-danger'>No Todo Found!</h1>
+        <h4>Add your own!</h4>
+      </div>
     )
+  }
+
+  return (
+    <div className='listWrap'>
+      <ul className='list-group'>
+        {todos &&
+          todos.length > 0 &&
+          todos.map((todo) => (
+            <ListItems
+              key={todo.id}
+              id={todo.id}
+              title={todo.title}
+              done={todo.done}
+              delHandler={delHandler}
+              doneHandler={doneHandler}
+            />
+          ))}
+      </ul>
+    </div>
+  )
 }
 
-export default List;
+export default Lists
